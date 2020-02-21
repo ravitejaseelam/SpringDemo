e1373d35f1ee7774739a0eedcdb712b8439222e1
RAVITEJA <raviteja.seelam@pramati.com>
2020-02-21 16:31:16 +0530

    API Calls Explored

--- src/main/java/com/example/demo/web/UserController.java
+++ src/main/java/com/example/demo/web/UserController.java
@@ -3,44 +3,43 @@ package com.example.demo.web;
 import com.example.demo.User;
 import com.example.demo.UserManager;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/Users")
 public class UserController {
 @Autowired private UserManager taskManager;
 
-    @RequestMapping("/AddMember/{name}/{description}")
+    @PostMapping("/{name}/{description}")
     public String addUser(@PathVariable String name, @PathVariable String description) {
         taskManager.add(name, description);
         return name+" is added";
     }
-    @RequestMapping("/SearchByName/{name}")
+    @GetMapping("/SearchByName/{name}")
     public List<User> searchByName(@PathVariable String name){
         return taskManager.searchByName(name);
     }
-    @RequestMapping("/SearchById/{Id}")
+    @GetMapping("/SearchById/{Id}")
     public List<User> searchById(@PathVariable Integer Id){
         return taskManager.searchById(Id);
     }
-    @RequestMapping("/DeleteById/{id}")
+    @DeleteMapping("/{id}")
     public String deleteById(@PathVariable Integer id){
         User obj=taskManager.deleteById(id);
         return obj.name+" is Deleted";
     }
-    @RequestMapping("/UpdateDescription/{id}/{description}")
+    @PutMapping("/UpdateDescription/{id}/{description}")
     public String updateDes(@PathVariable Integer id,@PathVariable String description){
         User obj=taskManager.updateDes(id,description);
         return obj.name+" is Updated";
     }
-    @RequestMapping("/DisplayAll")
+    @GetMapping
     public List<User> displayAll(){
         return taskManager.displayAll();
     }
-    @RequestMapping("/DeleteAll")
+    @DeleteMapping
     public String deleteAll(){
      taskManager.deleteAll();
      return "Data is Delete";
